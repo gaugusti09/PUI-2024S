3b892cdc13c3814f7aab3187864ae45635ca1e8c
G/A Augustin <gaugusti@andrew.cmu.edu>
2024-02-17 21:56:20 -0500

    In the funniest turn of events, the worst code I've ever written

--- solution-hw3/app.js
+++ solution-hw3/app.js
@@ -1,71 +1,104 @@
-// A list of cars with models and descriptions. We will create these
-// using a class in later assignments, but it's not necessary for
-// this simple example.
-let allCars = [
+let allGlazes = [
     {
-      model: 'Toyota Corolla',
-      description: 'A solid, reliable car.',
+      name: 'Keep original',
+      price: 0.00,
     },
     {
-      model: 'VW Beetle',
-      description: 'A compact, cute car.',
+      name: 'Sugar milk',
+      price: 0.00,
     },
     {
-      model: 'Chevy Corvette',
-      description: 'A cool car for cool people.',
+      name: 'Vanilla milk',
+      price: 0.50,
     },
+    {
+      name: 'Double Chocolate',
+      price: 1.50,
+    }
   ];
-  
-  /**
-   * Updates the UI to display a particular car's info.
-   * @param car A car object containing a model and a description.
-   */
-  function displayCar(car) {
-    let carTitleElement = document.querySelector('#car-title');
-    let carInfoElement = document.querySelector('#car-info');
-  
-    carTitleElement.innerText = car.model;
-    carInfoElement.innerText = car.description;
+
+
+let allSizes= [
+    {
+      quantity: '1',
+      adaptation: 1,
+    },
+    {
+      quantity: '3',
+      adaptation: 3,
+    },
+    {
+       quantity: '6',
+       adaptation: 5,
+    },
+    {
+       quantity: '12',
+       adaptation: 10,
+    }
+  ];
+
+
+function glazingPopulate() {
+    //populate glazing dropdowns
+    var glazeSel = document.getElementById('glazingOptions');
+    for(let i = 0; i < allGlazes.length; i++) {
+        let option =document.createElement('option');
+        option.innerHTML = allGlazes[i].name
+        glazeSel.appendChild(option)
+        //return allGlazes[i].price
+    };
+}
+
+function sizePopulate() {
+    //populate packsize dropdowns
+    var sizeSel = document.getElementById('packOptions');
+    for(var i = 0; i < allSizes.length; i++) {
+        let option = document.createElement('option');
+        option.innerHTML = allSizes[i].quantity
+        sizeSel.appendChild(option)
+        //return allSizes[i].adaptation
+    };
+}
+
+function updatePriceGl() {
+    var glazeSel = document.getElementById('glazingOptions');
+    var value = glazeSel.value;
+    var add = 0.00;
+    if (value != ("Keep original") && value != ("Sugar milk")){
+        if (value=="Vanilla milk") {
+            add = .5
+        } else {
+            add = 1.5
+        }
+    }
+
+    var packSel = document.getElementById('packOptions');
+    var value = packSel.value;
+    var size = 1;
+    if (value==3) {
+        size = 3;
+    } else {
+        if (value==6) {
+            size = 5;
+        } else {
+            size = 10;
+        }
+    }
+    console.log(add)
+    console.log(size)
+    let finalP = ((2.49 + add)*size);
+    let pricing = document.querySelector("#totalPrice");
+    pricing.innerText = "$" + finalP.toFixed(2);
+}
+
+glazingPopulate();
+sizePopulate();
+
+
+//update the price when either selector changes
+
+let elements = document.querySelectorAll('select');
+for (const element of elements) {
+    element.addEventListener('change', updatePriceGl);
   }
-  
-  function onSelectValueChange() {
-    // In this function, `this` corresponds to the select
-    // element. So `this.value` will contain the value of the
-    // selected option as a string.
-    console.log('You selected ' + this.value);
-  
-    // We need to convert the string value to an integer
-    let carIndex = parseInt(this.value);
-  
-    // Now retrieve the object at the index specified by the select's value
-    let carToDisplay = allCars[carIndex];
-  
-    // Update the UI
-    displayCar(carToDisplay);
-  }
-  
-  // When the page loads, find the select element.
-  let selectElement = document.querySelector('#car-select');
-  
-  // Let's add a new car to the allCars array.
-  let newCar = {
-    model: 'Honda Odyssey',
-    description: 'A practical minivan for soccer moms and everyone else.',
-  };
-  allCars.push(newCar);
-  
-  // We also need to add this new car to the UI. To do that, create a new
-  // 'option' HTML element, set its attributes, and add it to the select
-  // element.
-  var option = document.createElement('option');
-  option.text = newCar.model;
-  option.value = allCars.length - 1; // Its value should be the index of the last element in allCars
-  selectElement.add(option);
-  
-  // Give it a listener for the 'change' event, which is a function that will run
-  // when the selected option changes. You could also do this by setting the
-  // onchange property of selectElement, e.g. selectElement.onchange = ...
-  selectElement.addEventListener('change', onSelectValueChange);
-  
-  // Initially, display the first car
-  displayCar(allCars[0]);
+
