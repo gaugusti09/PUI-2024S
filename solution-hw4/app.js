a8877b7d70e25f089d76175d3c89cd74c02c5ea6
G/A Augustin <gaugusti@andrew.cmu.edu>
2024-02-28 01:09:54 -0500

    ?

--- solution-hw4/app.js
+++ solution-hw4/app.js
@@ -16,20 +16,28 @@ let basePrice = 2.49;
 let packSel = 1;
 let cart = [];
 
+
+
+
 //Populate the selects!!
 function glazingPopulate() {
-  const queryStr = new URLSearchParams(window.location.search);
-  const rollType = queryStr.get("roll");
+  const params = new URLSearchParams(window.location.search);
+  const rollType = params.get("roll");
   const rollPrice = rolls[rolltype]["basePrice"];
   const imgP = "images/products/" + roll[rollType]["img"];
 
   //update page ! ! !
-  const headerElement = document.querySelector("#cart-header-text"); // the header
+  const headerElement = document.querySelector("#cart-header-text");
   headerElement.innerText = rollType + ' Cinnamon Roll';
-  const cartBunImg = document.querySelector("#cinnamon-img"); // the image
+
+  const cartBunImg = document.querySelector("#cinnamon-img");
   cartBunImg.src = '..//assets/products/' + imgFile;
-  let cartPrice = document.querySelector("#base-price"); // price
+
+  let cartPrice = document.querySelector("#base-price"); //...?
   cartPrice.innerText = price;
+
+  // basePrice = parseFloat(price);
+	//updateTotalPrice()
 
 }
 
@@ -64,20 +72,60 @@ function packChange(elem) {
 glazingPopulate();
 sizePackPopulate(); //????
 
-function updatePrice() {
-  const glazeSel = glazingPrices[glazeOpt];
-  const basePrice = packPrices[packOpt];
-  // calculation goes here
-  // ????const totalPrice = ;
-}
 
-class CinnamonRoll {
-  constructor(rollSelect, glazingSelect, packSelect, basePrice) {
-      this.type = rollSelect
-      this.glazing =  glazingSelect;
-      this.size = packSelect;
-      this.basePrice = basePrice;
+//update the price when either selector changes
+
+let elements = document.querySelectorAll('select');
+for (const element of elements) {
+    element.addEventListener('change', updatePrice);
   }
-}
 
-// can push this....???????
+//read the URL
+  
+  const queryString = window.location.search;
+  const params = new URLSearchParams(queryString);
+  const rollType = params.get("roll");
+  let price = 0.00;
+  let imgFile = "";
+  
+  //Extract the current roll’s information (name, price, image path) 
+  
+  for (let i in rolls) {
+      if (rolls[i].name == rollType) {
+          price = rolls[i].basePrice;
+          imgFile = rolls[i].imageFile;}
+  }
+  
+  console.log(price)
+  console.log(imgFile)
+
+
+  function updatePrice() {
+    var glazeSel = document.getElementById('glazingOptions');
+    var value = glazeSel.value;
+    var add = 0.00;
+    
+    if (value != ("Keep original") && value != ("Sugar milk")){
+        if (value == "Vanilla milk") {
+            add = .5
+        } else {
+            add = 1.5
+        }
+    }
+
+    var packSel = document.getElementById('packOptions');
+    var value = packSel.value;
+    var size = 1;
+    if (value == 3) {
+        size = 3;
+    } else {
+        if (value == 6) {
+            size = 5;
+        } else if (value == 12) {
+          size = 10;
+        }
+    }
+    let finalP = (2.49 + add)*size;
+    let pricing = document.querySelector("#totalPrice");
+    pricing.innerText = "$" + finalP.toFixed(2);
+}
