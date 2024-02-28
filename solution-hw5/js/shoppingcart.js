173913ee5a10dfc4993bea7ea57c43b9c37c1e6e
G/A Augustin <gaugusti@andrew.cmu.edu>
2024-02-28 02:35:01 -0500

    Works on my computer but not website...?

--- solution-hw5/js/shoppingcart.js
+++ solution-hw5/js/shoppingcart.js
@@ -11,30 +11,49 @@ class Roll {
 }
 
 // Make Cinnamon Roll objects!
-originalRoll = new Roll('Original', 'Sugar Milk', 1, 2.49);
-walnutRoll = new Roll('Walnut', 'Vanilla Milk', 12, 3.49);
-raisinRoll = new Roll('Raisin', 'Sugar Milk', 3, 2.99);
-appleRoll = new Roll('Apple', 'Original', 3, 3.49);
+const originalRoll = new Roll('Original', 'Sugar Milk', 1, 2.49);
+const walnutRoll = new Roll('Walnut', 'Vanilla Milk', 12, 3.49);
+const raisinRoll = new Roll('Raisin', 'Sugar Milk', 3, 2.99);
+const appleRoll = new Roll('Apple', 'Original', 3, 3.49);
+
+// initially ! add the rolls to the cart
+function populateCart() {
+    console.log(cart);
+    cart.push(originalRoll);
+    cart.push(walnutRoll);
+    cart.push(raisinRoll);
+    cart.push(appleRoll)
+
+    console.log(cart);
+}
+
+console.log(2);
+populateCart();
 
 
 // price computation
 
 function appendRoll(givenRoll){
-    const imagePath = "images/products/" + rolls[givenRoll]["imageFile"];
-    const name = givenRoll.
-
-    // picture
-    // name
-    // glazing
-    // pack size
-    // item price to calculate
-    // remove item
+    cart.push(givenRoll);
+    let template = document.getElementById(givenRoll.type);
+    let templateContent = template.content;
+    document.body.appendChild(templateContent);
 }
+
+
+populateCart();
 
 function displayRolls() {
-    //
+    for (i=0; i<cart.length; i++) {
+        appendRoll(cart[i]);
+        console.log('heyyy');
+    }
 }
 
-function removeRoll(){
-    ///??
+function removeRoll(givenRoll){
+    cart.pop(givenRoll)
+    const element = document.getElementById(givenRoll.type);
+    element.remove();
 }
+
+console.log("Hello world!"); 
