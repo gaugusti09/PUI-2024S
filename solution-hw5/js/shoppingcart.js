8a9adf6dc65c3c0546b7ff93f6037fb20cc380c1
G/A Augustin <gaugusti@andrew.cmu.edu>
2024-02-28 01:52:04 -0500

    hw5

---
+++ solution-hw5/js/shoppingcart.js
@@ -0,0 +1,40 @@
+const cart = [];
+
+// Cinnamon Roll class from HW4
+class Roll {
+    constructor(rollType, rollGlazing, packSize, rollPrice) {
+        this.type = rollType;
+        this.glazing =  rollGlazing;
+        this.size = packSize;
+        this.basePrice = rollPrice;
+    }
+}
+
+// Make Cinnamon Roll objects!
+originalRoll = new Roll('Original', 'Sugar Milk', 1, 2.49);
+walnutRoll = new Roll('Walnut', 'Vanilla Milk', 12, 3.49);
+raisinRoll = new Roll('Raisin', 'Sugar Milk', 3, 2.99);
+appleRoll = new Roll('Apple', 'Original', 3, 3.49);
+
+
+// price computation
+
+function appendRoll(givenRoll){
+    const imagePath = "images/products/" + rolls[givenRoll]["imageFile"];
+    const name = givenRoll.
+
+    // picture
+    // name
+    // glazing
+    // pack size
+    // item price to calculate
+    // remove item
+}
+
+function displayRolls() {
+    //
+}
+
+function removeRoll(){
+    ///??
+}
